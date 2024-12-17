331703395125671139f8753c7d8777841c9beead
Adi Praditya <Leafxburst527@gmail.com>
2024-12-17 21:02:32 +0800

    PieChart + database

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produk;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view('dashboard');
+        $currentMonth = now()->month;
+        $currentYear = now()->year;
+
+        $totalSales = Produk::getMonthlySales($currentMonth, $currentYear);
+
+        return view('dashboard',['SalesData' => Produk::all(),'TotalSales'=>$totalSales]);
     }
 }
